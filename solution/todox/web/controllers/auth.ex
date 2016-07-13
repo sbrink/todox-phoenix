d7fa19d4dc8e61500ecf7d6a3ca4c9d9dea45d36
Karol Sluszniak <ksluszniak@gmail.com>
2016-07-13 13:21:51 +0200

    Implement auth logic

---
+++ solution/todox/web/controllers/auth.ex
@@ -0,0 +1,48 @@
+defmodule Todox.Auth do
+  import Plug.Conn
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        assign(conn, :current_user, user)
+      user = user_id && repo.get(Todox.User, user_id) ->
+        assign(conn, :current_user, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def login(conn, username, password, opts) do
+    repo = Keyword.fetch!(opts, :repo)
+    user = repo.get_by(Todox.User, username: username)
+
+    cond do
+      user && checkpw(password, user.password_hash) ->
+        {:ok, login(conn, user)}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        dummy_checkpw
+        {:error, :not_found, conn}
+    end
+  end
+
+  def logout(conn) do
+    delete_session(conn, :user_id)
+  end
+end
