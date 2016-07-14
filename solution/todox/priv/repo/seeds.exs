7e239ea573108876f89aa092eb26bbe6e9547b7b
Karol Sluszniak <ksluszniak@gmail.com>
2016-07-14 17:05:20 +0200

    Implement seeds

--- solution/todox/priv/repo/seeds.exs
+++ solution/todox/priv/repo/seeds.exs
@@ -1,11 +1,19 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Todox.Repo.insert!(%Todox.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+alias Todox.{ Repo, TodoItem }
+
+todo_items = [
+  %{ content: "Go to lunch" },
+  %{ content: "Waste some time" },
+  %{ content: "Work my ass off", done: true },
+  %{ content: "Work even more", done: true },
+  %{ content: "Work till dawn", done: true }
+]
+
+Enum.each todo_items, fn %{ content: content} = todo_item_params ->
+  todo_item = Repo.get_by(TodoItem, content: content)
+  todo_item ||
+    Repo.insert!(TodoItem.changeset(%TodoItem{ }, todo_item_params))
+end
