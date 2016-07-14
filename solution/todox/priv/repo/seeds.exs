c4be07b0178132a6b3a4f2b14850ffb190df9c49
Karol Sluszniak <ksluszniak@gmail.com>
2016-07-14 17:07:39 +0200

    Update seeds for multi-user system

--- solution/todox/priv/repo/seeds.exs
+++ solution/todox/priv/repo/seeds.exs
@@ -2,18 +2,36 @@
 #
 #     mix run priv/repo/seeds.exs
 
-alias Todox.{ Repo, TodoItem }
+alias Todox.{ Repo, TodoItem, User }
 
-todo_items = [
-  %{ content: "Go to lunch" },
-  %{ content: "Waste some time" },
-  %{ content: "Work my ass off", done: true },
-  %{ content: "Work even more", done: true },
-  %{ content: "Work till dawn", done: true }
+users = [
+  %{
+    username: "lazy-guy",
+    password: "seedpass",
+    todo_items: [
+      %{ content: "Go to lunch" },
+      %{ content: "Waste some time" }
+    ]
+  },
+
+  %{
+    username: "workaholic",
+    password: "seedpass",
+    todo_items: [
+      %{ content: "Work my ass off", done: true },
+      %{ content: "Work even more", done: true },
+      %{ content: "Work till dawn", done: true }
+    ]
+  }
 ]
 
-Enum.each todo_items, fn %{ content: content} = todo_item_params ->
-  todo_item = Repo.get_by(TodoItem, content: content)
-  todo_item ||
-    Repo.insert!(TodoItem.changeset(%TodoItem{ }, todo_item_params))
+Enum.each users, fn %{ username: username } = user_params ->
+  user = Repo.get_by(User, username: username)
+  user = user || Repo.insert!(User.registration_changeset(%User{}, user_params))
+
+  Enum.each user_params.todo_items, fn %{ content: content} = todo_item_params ->
+    todo_item = Repo.get_by(TodoItem, user_id: user.id, content: content)
+    todo_item ||
+      Repo.insert!(TodoItem.changeset(%TodoItem{ user: user }, todo_item_params))
+  end
 end
